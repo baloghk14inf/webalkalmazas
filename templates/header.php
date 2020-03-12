8969f42284756de774bef1b28ad6cefe3ff91ea1
baloghk14inf <60222483+baloghk14inf@users.noreply.github.com>
2020-03-12 06:48:35 +0100

    2020.03.12 mvc megkezdése

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a href="/" class="navbar-brand">TIMMT</a>
+    <ul class="nav navbar-nav ml-auto">
+        
+    <?php while($row = mysqli_fetch_array($result)):?> <!-- Ez itt elvileg jó de majd otthon inkább ellenőrizd-->
+
+         <li class="nav-item mr-3 "><a href="<?=$row['mnev']?>" class="nav-link text-info"><?=$row['mrout']?></a></li>
+
+    <?php endwhile;?>
+
+    </ul>
+    </nav>
--- templates/layout.php
+++ templates/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php require_once "templates/min.css.html"; ?>
+    <?php require_once "min.css.html"; ?>
 </head>
 <body>
     <?php
@@ -15,25 +15,21 @@
     $query = "SELECT  m.nev mnev, m.route mroute FROM elerheto_menupontok e_m, menupontok m  WHERE e_m ='$jogkor'";
     $result = mysqli_query($connection, $query);
 
-     if ($jogkor == "") {
+    if ($jogkor == "") {
          
-        header("Location: blabla"); //itt az átirányításnál majd még gondold át
+        require_once "header.php"; //itt az átirányításnál majd még gondold át
      }
-    else: ?>
+    else {
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a href="/" class="navbar-brand">TIMMT</a>
-    <ul class="nav navbar-nav ml-auto">
-        
-    <?php while($row = mysqli_fetch_array($result)):?> <!-- Ez itt elvileg jó de majd otthon inkább ellenőrizd-->
+        require_once "header.php";
+        require_once "$view.php"; 
+     }
+     if ($view == "/registration") {
+        require_once "$view.php"; 
+     }
+     
 
-         <li class="nav-item mr-3 "><a href="<?=$row['mnev']?>" class="nav-link text-info"><?=$row['mrout']?></a></li>
-
-    <?php endwhile;?>
-
-    </ul>
-    </nav>
-<?php endif;?>
+    ?>
     
 </body>
 </html>
